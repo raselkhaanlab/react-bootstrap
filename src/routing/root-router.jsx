43b061a9c6457714cb8ce0b9ca5e9920e3609d3b
Rasel khan <raselkhanlab@gmail.com>
2024-09-27 11:53:47 +0600

    feat: add automatic token refresh

--- src/routing/root-router.jsx
+++ src/routing/root-router.jsx
@@ -1,20 +1,20 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
-import {Login} from "../pages/login";
-import {Registration} from "../pages/registration";
-import {Dashboard} from "../pages/dashboard";
-import {NotFound} from "../pages/not-found"
-import {paths} from "../paths";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "../pages/login";
+import { Registration } from "../pages/registration";
+import { Dashboard } from "../pages/dashboard";
+import { NotFound } from "../pages/not-found";
+import { paths } from "../paths";
 export function RootRouter() {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
-            <Route path= {paths.auth.signIn} element={<Login />} />
-            <Route path= {paths.auth.signUp} element={<Registration />} />
-            <Route index element={<Dashboard />} />
-            <Route path="*" element={<NotFound />} />
+          <Route path={paths.auth.signIn} element={<Login />} />
+          <Route path={paths.auth.signUp} element={<Registration />} />
+          <Route index element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
