ff4ddb4cc0fe75b99e1b75b743a587c6f235690f
Rasel khan <raselkhanlab@gmail.com>
2024-09-27 18:50:39 +0600

    remove un-nessasary console.log

--- src/components/auth/sign-up-form.jsx
+++ src/components/auth/sign-up-form.jsx
@@ -1,21 +1,23 @@
 import { Form, Button, Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { paths } from "../../paths";
-import { z as zod } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Controller,useForm } from "react-hook-form";
-import {getAuthClient} from "../../services/auth";
-import {useUser} from "../../hooks/use-user";
-import {useNavigate} from "react-router-dom";
+import { z as zod } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { getAuthClient } from "../../services/auth";
+import { useUser } from "../../hooks/use-user";
+import { useNavigate } from "react-router-dom";
 import { useCallback, useState } from "react";
 
 const schema = zod.object({
-  name: zod.string().min(1, { message: 'Name is required' }),
-  email: zod.string().min(1, { message: 'Email is required' }).email(),
-  password: zod.string().min(1, { message: 'Password should be at least 1 characters' }),
+  name: zod.string().min(1, { message: "Name is required" }),
+  email: zod.string().min(1, { message: "Email is required" }).email(),
+  password: zod
+    .string()
+    .min(1, { message: "Password should be at least 1 characters" }),
 });
 
-const defaultValues = { name: '', email: '', password: '' }
+const defaultValues = { name: "", email: "", password: "" };
 
 export function SignUpForm() {
   const navigate = useNavigate();
@@ -34,11 +36,10 @@ export function SignUpForm() {
   const onSubmit = useCallback(
     async (values) => {
       setIsPending(true);
-      const res= await getAuthClient().signUp(values);
-      console.log({res})
-      const {error} = res;
+      const res = await getAuthClient().signUp(values);
+      const { error } = res;
       if (error) {
-        setError('root', { type: 'server', message: error });
+        setError("root", { type: "server", message: error });
         setIsPending(false);
         return;
       }
@@ -64,38 +65,45 @@ export function SignUpForm() {
           </Link>
         </p>
       </div>
-      {errors.root ? <Alert variant="danger">{errors.root.message}</Alert> : null}
+      {errors.root ? (
+        <Alert variant="danger">{errors.root.message}</Alert>
+      ) : null}
       {/* Form Section */}
-      <Form onSubmit={handleSubmit(onSubmit, (data)=> console.log({data}))}>
+      <Form onSubmit={handleSubmit(onSubmit, (data) => console.log({ data }))}>
         {/* First Name */}
         <Form.Group className="mb-3">
           <Controller
-                  control={control}
-                  name="name"
-                  render={({field})=> (<>
-                      <Form.Label className="text-secondary fw-bold">
-                      {" "}
-                      {/* small and fw-bold to mimic MUI label */}
-                      Name
-                    </Form.Label>
-                    <Form.Control
-                      {...field}
-                      type="text"
-                      placeholder="Enter your name"
-                      className="form-control-md p-3"
-                    />
-                    {errors.name ? <Alert variant="danger">{errors.name.message}</Alert> : null}
-                </>)}
-              />
+            control={control}
+            name="name"
+            render={({ field }) => (
+              <>
+                <Form.Label className="text-secondary fw-bold">
+                  {" "}
+                  {/* small and fw-bold to mimic MUI label */}
+                  Name
+                </Form.Label>
+                <Form.Control
+                  {...field}
+                  type="text"
+                  placeholder="Enter your name"
+                  className="form-control-md p-3"
+                />
+                {errors.name ? (
+                  <Alert variant="danger">{errors.name.message}</Alert>
+                ) : null}
+              </>
+            )}
+          />
         </Form.Group>
 
         {/* Email */}
         <Form.Group className="mb-3">
           <Controller
-              control={control}
-              name="email"
-              render={({field})=> (<>
-                  <Form.Label className="text-secondary fw-bold">
+            control={control}
+            name="email"
+            render={({ field }) => (
+              <>
+                <Form.Label className="text-secondary fw-bold">
                   {" "}
                   {/* small and fw-bold to mimic MUI label */}
                   Email address
@@ -106,18 +114,22 @@ export function SignUpForm() {
                   placeholder="Enter your email"
                   className="form-control-md p-3"
                 />
-                {errors.email ? <Alert variant="danger">{errors.email.message}</Alert> : null}
-            </>)}
+                {errors.email ? (
+                  <Alert variant="danger">{errors.email.message}</Alert>
+                ) : null}
+              </>
+            )}
           />
         </Form.Group>
 
         {/* Password */}
         <Form.Group className="mb-3">
-        <Controller
-              control={control}
-              name="password"
-              render={({field})=> (<>
-                  <Form.Label className="text-secondary fw-bold">
+          <Controller
+            control={control}
+            name="password"
+            render={({ field }) => (
+              <>
+                <Form.Label className="text-secondary fw-bold">
                   {" "}
                   {/* small and fw-bold to mimic MUI label */}
                   Password
@@ -128,12 +140,20 @@ export function SignUpForm() {
                   placeholder="Enter your password"
                   className="form-control-md p-3"
                 />
-                {errors.password ? <Alert variant="danger">{errors.password.message}</Alert> : null}
-            </>)}
+                {errors.password ? (
+                  <Alert variant="danger">{errors.password.message}</Alert>
+                ) : null}
+              </>
+            )}
           />
         </Form.Group>
         {/* Submit Button */}
-        <Button disabled={isPending} className="w-100" variant="primary" type="submit">
+        <Button
+          disabled={isPending}
+          className="w-100"
+          variant="primary"
+          type="submit"
+        >
           Sign up
         </Button>
       </Form>
--- src/components/dashboard/layout/main-nav.jsx
+++ src/components/dashboard/layout/main-nav.jsx
@@ -1,18 +1,13 @@
-import {
-  Navbar,
-  Nav,
-  OverlayTrigger,
-} from "react-bootstrap";
+import { Navbar, Nav, OverlayTrigger } from "react-bootstrap";
 import MenuToogleIcon from "../../icons/hamburger.svg?react";
-import { usePopover } from '../../../hooks/use-popover';
+import { usePopover } from "../../../hooks/use-popover";
 import { UserPopover } from "./user-popover";
 import { useUser } from "../../../hooks/use-user";
 import { getUserFirstAndLastChar } from "../../../utils/user";
 
 export function MainNav({ onToggle }) {
-  const {user} = useUser();
+  const { user } = useUser();
   const userPopover = usePopover();
-  console.log(user);
   return (
     <Navbar
       style={{ minHeight: 65 }}
@@ -23,18 +18,26 @@ export function MainNav({ onToggle }) {
     >
       <div className="container-fluid d-flex justify-content-between align-items-center px-5">
         <Nav className="d-flex align-items-center">
-          <MenuToogleIcon height={24} width={24} onClick={() => onToggle()} role="button" className="d-block d-lg-none" />
+          <MenuToogleIcon
+            height={24}
+            width={24}
+            onClick={() => onToggle()}
+            role="button"
+            className="d-block d-lg-none"
+          />
         </Nav>
         <OverlayTrigger
           trigger="click"
           placement="bottom"
-          overlay={<UserPopover className="" onClose={userPopover.handleClose}/>}
+          overlay={
+            <UserPopover className="" onClose={userPopover.handleClose} />
+          }
           rootClose
         >
           <div className="d-flex flex-column">
             <p
               role="button"
-              style={{height:44, width:44}}
+              style={{ height: 44, width: 44 }}
               onClick={userPopover.handleToggle}
               className="p-1 align-self-end rounded-circle d-flex align-items-center justify-content-center bg-primary text-white text-center my-auto"
             >
@@ -47,5 +50,3 @@ export function MainNav({ onToggle }) {
     </Navbar>
   );
 }
-
-
